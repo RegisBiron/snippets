45716f199a3e2ed3040f5fce2098ca150c9a8a5f
Regis Biron <regis@might-main.com>
2014-11-19 13:45:13 -0500

    mobile test js snippet

---
+++ js/js-mobile-test.js
@@ -0,0 +1,11 @@
+//test for a mobile/tablet device
+
+var isMobile = false;
+
+if(navigator.userAgent.match(/(android|iphone|ipad|blackberry|symbian|symbianos|symbos|netfront|model-orange|javaplatform|iemobile|windows phone|samsung|htc|opera mobile|opera mobi|opera mini|presto|huawei|blazer|bolt|doris|fennec|gobrowser|iris|maemo browser|mib|cldc|minimo|semc-browser|skyfire|teashark|teleca|uzard|uzardweb|meego|nokia|bb10|playbook)/gi)){
+	isMobile = true;
+}
+
+if(!isMobile){
+	//not a mobile/tablet device
+}
